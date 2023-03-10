29e44ae9f17f16ab708d499ae8d127f6eccb2eb6
Rick Arcanjo <Ricardo.cassettari07@gmail.com>
2023-03-10 14:49:40 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Beba-Coca-Cola");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write(@"     beba  coca  cola
+     babe        cola
+     beba  coca
+     babe  cola  caco
+     caco
+     cola
+          c l o a c a
+          ");
+          Console.ResetColor();
+
